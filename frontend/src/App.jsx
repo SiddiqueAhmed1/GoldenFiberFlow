9d5deb90bd331f76f86f2425509b3221206b12e9
Siddique Ahmed <developersiddique123@gmail.com>
2026-04-12 06:34:37 +0600

    feat: responsive login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import { ToastContainer } from "react-toastify";
 import "@fontsource/inter";
 import "@fontsource/poppins";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/login");
+  }, []);
+
   return (
     <>
       <ToastContainer />
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -5,8 +5,9 @@ export const Login = () => {
   return (
     <>
       <section className="flex items-center justify-center h-screen bg-blue-50">
-        <div className="shadow-md drop-shadow-lg shadow-amber-500/10 bg-white rounded-2xl border border-amber-50 w-550 h-700 m-auto px-9 py-11">
-          <div className="loing-metadata flex flex-col justify-center items-center gap-1">
+        <div className=" shadow-md drop-shadow-lg shadow-amber-500/10 bg-white rounded-2xl border border-amber-50 w-85 h-150 lg:w-[500px] lg:h-[700px] m-auto px-6 pt-5 lg:px-9  lg:py-11">
+          {/* loing-metadata */}
+          <div className=" flex flex-col justify-center items-center gap-1">
             <img
               className="mb-3"
               height={70}
@@ -15,17 +16,18 @@ export const Login = () => {
               alt="GFTCL"
             />
             <h1 className="">Welcome Back</h1>
-            <p className="text-lg text-neutral-600 font-medium">
+            <p className="text-center lg:w-full w-40 text-sm lg:text-lg text-neutral-600 font-medium">
               Enter your credentials to access your account
             </p>
           </div>
 
-          <div className="my-8">
+          {/* login form */}
+          <div className="my-4 lg:my-6">
             <form className="flex flex-col gap-3">
               <div className="flex flex-col gap-1">
                 <label htmlFor="email">Email</label>
                 <input
-                  className="focus-within:outline-amber-500 focus-within:outline-4"
+                  className="focus-within:outline-amber-500 focus-within:outline-2 h-11 lg:h-[52px] w-full rounded-lg border outline-0 border-neutral-300 px-3 lg:text-lg text-sm"
                   id="email"
                   type="text"
                   placeholder="admin@example.com"
@@ -34,28 +36,35 @@ export const Login = () => {
               <div className="flex flex-col gap-1">
                 <label htmlFor="password">Password</label>
                 <input
+                  className="focus-within:outline-amber-500 focus-within:outline-2 h-11 lg:h-[52px] w-full rounded-lg border outline-0 border-neutral-300 px-3 lg:text-lg text-sm"
                   id="password"
                   type="password"
                   placeholder="Enter your password"
                 />
               </div>
-              <div className="border border-amber-300 bg-amber-50 px-4 py-3 mt-2 rounded-lg ">
-                <h2 className="text-amber-800 text-lg">Demo credentials:</h2>
-                <p className="text-amber-700 text-sm">
+              {/* demo credentials */}
+              <div className="border border-amber-300 bg-amber-50 px-2 py-1 lg:px-4 lg:py-3 lg:mt-2 rounded-lg ">
+                <h2 className="text-amber-800 text-sm lg:text-lg">
+                  Demo credentials:
+                </h2>
+                <p className="text-amber-700 text-xs lg:text-sm">
                   Email: admin@example.com
                 </p>
-                <p className="text-amber-700 text-sm">Password: admin123</p>
+                <p className="text-amber-700 text-xs lg:text-sm">
+                  Password: admin123
+                </p>
               </div>
-              <div className="flex flex-col mt-2">
-                <button className="bg-amber-400 py-3 rounded-lg text-white font-semibold cursor-pointer hover:bg-amber-500 transition-all">
+              <div className="flex flex-col lg:mt-2">
+                <button className="bg-amber-400 py-2 lg:py-3 text-sm lg:text-lg rounded-lg text-white lg:font-semibold cursor-pointer hover:bg-amber-500 transition-all">
                   Sign In
                 </button>
               </div>
             </form>
           </div>
+
           {/* form footer */}
-          <div className="flex justify-end">
-            <p>
+          <div className="flex lg:justify-end justify-center ">
+            <p className="lg:text-sm text-xs">
               Don't have account?{" "}
               <NavLink
                 className="text-amber-500 font-semibold hover:underline"
