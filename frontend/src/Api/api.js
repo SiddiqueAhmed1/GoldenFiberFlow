e7deb0770eea87a0185539be23c98bcfea0d9a8c
Siddique Ahmed <developersiddique123@gmail.com>
2026-04-25 15:34:55 +0600

    bug: log meta data

--- frontend/src/Api/api.js
+++ frontend/src/Api/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import toast from "react-hot-toast";
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 // create cusome axios
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
