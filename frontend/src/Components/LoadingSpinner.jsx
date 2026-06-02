b26291242dd796f36caa73925398c14fd408b11d
Siddique Ahmed <developersiddique123@gmail.com>
2026-06-02 15:44:27 +0600

    feat: changed favicon

--- frontend/src/Components/LoadingSpinner.jsx
+++ frontend/src/Components/LoadingSpinner.jsx
@@ -1,16 +1,7 @@
-export default function LoadingSpinner({
-  border = "primary",
-  color = "text-muted-foreground",
-  text = "Loading...",
-}) {
+export default function LoadingSpinner() {
   return (
-    <div className="flex items-center justify-center py-20">
-      <div className="text-center space-y-3">
-        <div
-          className={`w-8 h-8 border-2 border-${border} border-t-transparent rounded-full animate-spin mx-auto`}
-        />
-        <p className={`text-sm text-${color} `}>{text}</p>
-      </div>
+    <div className="flex items-center justify-center h-full min-h-96">
+      <div className="w-8 h-8 border-4 border-amber-500 border-t-transparent rounded-full animate-spin" />
     </div>
   );
 }
